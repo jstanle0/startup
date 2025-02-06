85e615c99cdfcae03f0fef291ea3e395452be99d
jstanle0 <jstanle0@byu.edu>
2025-02-06 14:01:22 -0700

    login react

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import './login.css'
 
 export function Login() {
-    return <main className='container-fluid bg-secondary text-center'>
-    <div>login displayed here</div>
-  </main>
+    return <main className="form-signin w-100 m-auto">
+    <div className="content-container">
+        <form>
+            <h3>Welcome to Shoot for the Stars!!</h3>
+            <div className="form-floating">
+                <input id="floatingInput" className="form-control" placeholder="username"/>
+                <label for="floatingInput">Username</label>
+            </div>
+            <div className="form-floating">
+                <input id="floatingPassword" className="form-control" placeholder="Password"/>
+                <label for="floatingPassword">Password</label>
+            </div>
+            <a href="html/main.html"><button>Login</button></a>
+        </form>
+    </div>
+</main>;
 }
