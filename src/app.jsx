ebe23ec127914e04ff4f69d845f03693d48444c2
jstanle0 <jstanle0@byu.edu>
2025-02-06 10:28:37 -0700

    added header

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className='body bg-dark text-light'>App will display here</div>;
+  return <div className='app'>
+    <Header />
+  </div>;
+}
+
+function Header() {
+    return <header>
+        <div class="aligned-header">
+            <a href="../html/main.html" class="logo"><img src="../public/images/logo.png" alt="logo" height="75"/></a>
+            <h1>Shoot for the Stars!</h1>
+            <nav>
+                <a href="../index.html">Login</a> 
+                <a href="../html/about.html">About</a> 
+                <a href="../html/community.html">Community</a> 
+            </nav>
+        </div> 
+    </header>;
 }
