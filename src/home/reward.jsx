7895b295e4b080217b9f7992201844bec99d8dc6
jstanle0 <jstanle0@byu.edu>
2025-02-20 09:15:48 -0700

    option to complete reward

--- src/home/reward.jsx
+++ src/home/reward.jsx
@@ -4,8 +4,23 @@ import { starCountContext } from './home';
 
 export function DisplayReward() {
     const {starCount, setStarCount} = React.useContext(starCountContext)
+    const [rewardExists, setRewardExists] = React.useState(false)
 
     function Reward({image, title, caption, starValue}) {
+        function completeReward() {
+            setStarCount(starCount - starValue)
+            setRewardExists(false)
+        }
+        function ProgressBar() {
+            if (starCount >= starValue) {
+                return <button type="button" className="btn btn-secondary btn-lg" style={{"maxWidth": "200px"}} onClick={()=>completeReward()} >Complete!</button>
+            } else {
+                return <div className="progress" role="progressbar" aria-label="Basic example" aria-valuenow={starCount} aria-valuemin="0" aria-valuemax={starValue}>
+                <div className='progress-bar' style={{width: ((starCount/starValue) * 100).toFixed(2) + '%'}}></div>
+            </div>
+                
+            }
+        }
     return <>
     {/*<div className='card bg-primary-subtle mb-3'>
       <img src={image} className="card-img-top" alt="reward image"/>
@@ -18,7 +33,7 @@ export function DisplayReward() {
           </div>
       </div>
     </div>*/}
-    <div className="card mb-3 bg-primary-subtle" style={{maxWidth: '540px'}}>
+    <div className="card mb-3" style={{maxWidth: '540px', border: 'none'}}>
         <div className="row g-0">
             <div className="col-md-4 bg-primary rounded-start" style={{display: 'flex', alignItems: 'center'}}>
             <img src="/images/ferrari.jpeg" className="img-fluid rounded-start" alt="ferrari"/>
@@ -28,19 +43,85 @@ export function DisplayReward() {
                     <h5 className="card-title bg-warning">{title}</h5>
                     <p className="card-text bg-warning">{caption}</p>
                     <p className="card-text bg-warning">{starCount}/{starValue} <img alt="star" src="/images/star.png" height="15" className='bg-warning'></img></p>
-                    <div className="progress" role="progressbar" aria-label="Basic example" aria-valuenow={starCount} aria-valuemin="0" aria-valuemax={starValue}>
-                        <div className='progress-bar' style={{width: ((starCount/starValue) * 100).toFixed(2) + '%'}}></div>
-                    </div>
+                    <ProgressBar />
                 </div>
             </div>
         </div>
     </div>
     </>
     }
+    function RewardModal() {
+        
+            const [modalIsOpen, setIsOpen] = React.useState(false);
+          
+            function openModal() {
+                setIsOpen(true);
+            }
+          
+            function closeModal() {
+                setIsOpen(false);
+            }
+          
+            const customStyles = {
+                content: {
+                top: '50%',
+                left: '50%',
+                right: 'auto',
+                bottom: 'auto',
+                marginRight: '-50%',
+                transform: 'translate(-50%, -50%)',
+                backgroundColor: '#FAFAFF',
+                },
+            };
+              
+                /*function afterOpenModal() {
+                  // references are now sync'd and can be accessed.
+                  subtitle.style.color = '#f00';
+                }*/
+            const handleSubmit = () => {
+            setRewardExists(true)
+            }
+    
+            const [goalName, setGoalName] = React.useState("");
+            const [goalDesc, setGoalDesc] = React.useState("");
+            const [starValue, setStarValue] = React.useState("")
+        
+        return (
+            <div>
+            <button type="button" className="btn btn-secondary btn-lg" style={{"maxWidth": "200px"}} onClick={openModal}>Create a reward!</button>
+
+            <Modal
+                isOpen={modalIsOpen}
+                //onAfterOpen={afterOpenModal}
+                onRequestClose={closeModal}
+                style={customStyles}
+                contentLabel="Goal Modal"
+            >
+                <form>
+                    <div className="mb-3">
+                        <label htmlFor="goalInput" className="form-label">Goal</label>
+                        <input type="goal" className="form-control" id="goalInput" value={goalName} onChange={(e) => setGoalName(e.target.value)} required autoComplete="off"/>
+                    </div>
+                    <div class="mb-3">
+                        <label htmlFor="descriptionInput" className="form-label">Description</label>
+                        <input type="description" className="form-control" id="descriptionInput" value={goalDesc} onChange={(e) => setGoalDesc(e.target.value)} required autoComplete="off"/>
+                    </div>
+                    <div class="mb-3">
+                        <label htmlFor="starCountInput" className="form-label">Star Count</label>
+                        <input type="number" className="form-control" id="starCountInput" value={starValue} onChange={(e)=> setStarValue(e.target.value)} required autoComplete="off"/>
+                    </div>
+                    <button type="submit" className="btn btn-primary" onClick={()=>handleSubmit() }>Submit</button>
+                    </form>
+            </Modal>
+            </div>
+        );
+        
+    }
+    if (rewardExists) {
+        return  <Reward image="/images/ferrari.jpeg" title="ferrari" caption="i want ferrari pls" starValue={20}/>
+
+    } else {
     return (
-        <>
-        <Reward image="/images/ferrari.jpeg" title="ferrari" caption="i want ferrari pls" starValue={20}/>
-        <button type="button" className="btn btn-secondary btn-lg" style={{"maxWidth": "200px"}}>Create new Reward!</button>
-        </>
+        <RewardModal/>
     )
-  }
+  }}
