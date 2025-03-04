04c21d1ec38c0772f10262b27705f40c3050664f
jstanle0 <jstanle0@byu.edu>
2025-03-04 10:42:40 -0700

    service now active :)

--- service/service.js
+++ service/service.js
@@ -1,1 +1,16 @@
 const express = require('express')
+const app = express()
+
+app.use(express.json());
+
+app.use(express.static('public'))
+
+app.use(function (err, req, res, next) {
+    res.status(500).send({ type: err.name, message: err.message });
+});
+
+app.use((_req, res) => {
+    res.sendFile('index.html', { root: './' });
+});
+
+app.listen(3000, () => { console.log("listening on port 3000") });
